064170b322210ca10fbf63516880249e5238a05c
AlankritaChauhan <90062945+AlankritaChauhan@users.noreply.github.com>
2022-02-10 19:22:01 +0530

    Add files via upload

---
+++ Milestone-2/Triangle/triangle.py
@@ -0,0 +1,17 @@
+import turtle
+
+tri=turtle.Turtle()
+
+tri.color("black")
+
+tri.begin_fill()
+tri.forward(100)
+
+tri.left(120)
+tri.forward(100)
+
+tri.left(120)
+tri.forward(100)
+tri.end_fill()
+
+turtle.done()
